7fe55708dc07f1a76da033acfa8ec4f748d36091
AndualemGeta <85870671+AndualemGeta@users.noreply.github.com>
2021-06-22 20:39:13 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,16 @@
-
+#include "holberton.h"
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase 10 times
+* @void - no argument
+*/
+void print_alphabet_x10(void)
+{
+char letter;
+int i;
+for (i = 1; i <= 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
+}
