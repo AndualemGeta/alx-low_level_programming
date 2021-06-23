df8b4f602943c28aad29beaf020501c872fd0234
AndualemGeta <85870671+AndualemGeta@users.noreply.github.com>
2021-06-23 22:27:52 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num;
+char letter;
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
