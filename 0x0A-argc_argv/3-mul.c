77c93644e1876e04140c48ccecfc492f21d0d29b
AndualemGeta <andualemgetachew80@gmail.com>
2021-07-12 05:38:20 -0700

    arg work

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Multiply 2 number if that numbers are digits.
+ * @argc: Size of my Array of arrays
+ * @argv: Position in my main array.
+ * Return: Always is 0 if is succes.
+ */
+int main(int argc, char **argv)
+{
+int a, res;
+
+res = 1;
+if (argc > 2)
+{
+for (a = 1; a < argc; a++)
+{
+res = res * atoi(argv[a]);
+}
+printf("%d\n", res);
+}
+else
+printf("Error\n");
+return (0);
+}
