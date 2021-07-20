7f40fd7c5618fbd9ee4947ebed87fef1adbef356
AndualemGeta <85870671+AndualemGeta@users.noreply.github.com>
2021-07-20 04:37:59 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,16 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *x;
+dog_t *x;
 
-	x = malloc(sizeof(dog_t));
+x = malloc(sizeof(dog_t));
 
-	if (x == NULL)
-		return (NULL);
+if (x == NULL)
+return (NULL);
 
-	x->name = name;
-	x->age = age;
-	x->owner = owner;
+x->name = name;
+x->age = age;
+x->owner = owner;
 
-	return (x);
+return (x);
 }
