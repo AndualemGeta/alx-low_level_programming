7ffd1b747382c40778339ab6bc9a4137722f9502
AndualemGeta <andualemgetachew80@gmail.com>
2021-08-11 10:05:23 -0700

    fix textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,61 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _strlen - count len of string
- * @s: string pointer
- * Return: len of string
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
-int _strlen(char *s)
-{
-int len = 0;
-
-while (s[len] != '\0')
-len++;
-return (len);
-}
-/**
- * read_textfile - function read textfiles
- * @filename: pointer namefile
- * @letters: letters for print
- * Return: print letters
- */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t value;
-char *buffer;
-int file, reading;
+	ssize_t o, r, w;
+	char *buffer;
 
-buffer = malloc(sizeof(char) * letters);
-file = open(filename, O_RDONLY);
-if (file == -1)
-{
-return (0);
-}
-if (!buffer)
-{
-free(buffer);
-return (0);
-}
-reading = read(file, buffer, letters);
-if (reading == -1)
-{
-return (0);
-}
-value = _strlen(buffer);
-value = write(STDOUT_FILENO, buffer, value);
-if (value == -1)
-{
-free(buffer);
-return (0);
-}
-free(buffer);
-if (close(file) == -1)
-{
-return (-1);
-}
-else
-{
-return (value);
-}
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
