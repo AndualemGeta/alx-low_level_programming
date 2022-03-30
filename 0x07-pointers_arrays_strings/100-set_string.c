f3f138c6edd1bf9dc0c39f18988c16796a48c745
AndualemGeta <andualemgetachew80@gmail.com>
2022-03-30 03:01:45 -0700

    last fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,10 @@
+#include "holberton.h"
 /**
- * set_string - Sets the value of a pointer to a char
- * @s: Pointer to change
- * @to: Char to asign
- *
- * Return: Nothing
+ * set_string -  sets a string
+ * @s: string to set
+ * @to: value to set to
  */
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
