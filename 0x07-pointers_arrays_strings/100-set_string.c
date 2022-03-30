511ebfbf09c4cfbcc5c8b2005df5ce093aed8f2f
AndualemGeta <andualemgetachew80@gmail.com>
2022-03-30 02:49:37 -0700

    string fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
+#include "holberton.h"
 /**
 * set_string - Sets the value of a pointer to a char
 * @s: Pointer to change
 * @to: Char to asign
-*
 * Return: Nothing
 */
 void set_string(char **s, char *to)
